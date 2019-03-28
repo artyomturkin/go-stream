c4954f9e994994a0e16135da967509f34b9d4967
Туркин Артём <artemtu@halykbank.kz>
2019-03-28 17:36:46 +0600

    add logging in kafka provider [temp]

--- providers/kafka/stream.go
+++ providers/kafka/stream.go
@@ -3,6 +3,8 @@ package kafka
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/artyomturkin/go-stream"
@@ -67,39 +69,64 @@ type consumer struct {
 }
 
 func (c *consumer) Read(ctx context.Context) (*stream.Message, error) {
+	log.Printf("[KAFKA] Read - acquire thread")
 	err := c.s.Acquire(ctx, 1)
 	if err != nil {
 		return nil, err
 	}
 
+	log.Printf("[KAFKA] Read - thread acquired")
+
 	for {
+		log.Printf("[KAFKA] Read - read message")
+
 		m, err := c.r.FetchMessage(ctx)
 		if err != nil {
+			log.Printf("[KAFKA] Read - read error: %v", err)
 			return nil, err
 		}
 
+		log.Printf("[KAFKA] Read - partition %d", m.Partition)
 		msg, err := stream.UnmarshalMessage(m.Value, c.uc)
 
 		if err == nil {
+			log.Printf("[KAFKA] Read - forward message: %v", msg)
 			msg.StreamMeta = m
 			return msg, nil
 		}
 
+		log.Printf("[KAFKA] Read - unmarshaling failed: %v", err)
 		if c.breakOnFormatError {
 			return nil, err
+		}
+
+		log.Printf("[KAFKA] Read - skipping")
+		err = c.r.CommitMessages(ctx, m)
+		if err != nil {
+			log.Printf("[KAFKA] Read - skipping failed: %v", err)
+			return nil, fmt.Errorf("failed to commit skipped message")
 		}
 	}
 }
 func (c *consumer) Ack(ctx context.Context, m *stream.Message) error {
-	c.s.Release(1)
+	log.Printf("[KAFKA] [%s] Ack - release thread", m.ID)
+	defer c.s.Release(1)
 	if msg, ok := m.StreamMeta.(k.Message); ok {
-		return c.r.CommitMessages(ctx, msg)
+		err := c.r.CommitMessages(ctx, msg)
+		if err != nil {
+			log.Printf("[KAFKA] [%s] Ack - error: %v", m.ID, err)
+			return err
+		}
+		log.Printf("[KAFKA] [%s] Ack - success", m.ID)
+		return nil
 	}
 
+	log.Printf("[KAFKA] [%s] Ack - StreamMeta is not formated correctly", m.ID)
 	return errors.New("StreamMeta is not formated correctly")
 }
 
 func (c *consumer) Nack(context.Context, *stream.Message) error {
+	log.Printf("[KAFKA] Nack - release thread")
 	c.s.Release(1)
 	return nil
 }
@@ -116,10 +143,19 @@ type producer struct {
 func (p *producer) Publish(ctx context.Context, m *stream.Message) error {
 	data, err := stream.MarshalMessage(m, p.uc)
 	if err != nil {
+		log.Printf("[KAFKA] [%s] Publish - failed to marshal message: %v", m.ID, err)
 		return err
 	}
 
-	return p.w.WriteMessages(ctx, k.Message{Value: data})
+	log.Printf("[KAFKA] [%s] Publish - message: %1000s", m.ID, string(data.([]byte)))
+	err = p.w.WriteMessages(ctx, k.Message{Value: data.([]byte)})
+	if err != nil {
+		log.Printf("[KAFKA] [%s] Publish - failed: %v", m.ID, err)
+		return err
+	}
+
+	log.Printf("[KAFKA] [%s] Publish - success", m.ID)
+	return nil
 }
 
 func (p *producer) Close() error {
